cca7bfc30f43059b58088c97854b246d0bf0fb59
Max <maximlev01234@gmail.com>
2020-10-25 23:37:02 +0200

    tried to fix my CORS, but error was on front side((((

--- src/main/java/ua/lviv/iot/rest_for_web_labs/controller/CarController.java
+++ src/main/java/ua/lviv/iot/rest_for_web_labs/controller/CarController.java
@@ -1,5 +1,6 @@
 package ua.lviv.iot.rest_for_web_labs.controller;
 
+import com.sun.net.httpserver.Headers;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,27 +14,31 @@ import java.util.List;
 @RequestMapping("cars")
 @AllArgsConstructor
 //@CrossOrigin(origins = "http://localhost:63343/*", maxAge = 3600)
-@CrossOrigin(value = "*", origins = "*", allowedHeaders = "*", maxAge = 3L)
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 public class CarController {
     private final CarService service;
 
+//    @CrossOrigin
     @GetMapping
     public ResponseEntity<List<Car>> getAllCars() {
         return service.getAllCars();
     }
 
+//    @CrossOrigin
     @PostMapping
     public ResponseEntity<Car> addCar(@RequestBody Car car) {
         service.addCar(car);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+//    @CrossOrigin
     @DeleteMapping("/{id}")
     public ResponseEntity<Car> deleteCar(@PathVariable("id") Integer id){
         service.deleteCar(id);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+//    @CrossOrigin
     @PutMapping("/{id}")
     public ResponseEntity<Car> updateCar(@PathVariable("id") Integer id, @RequestBody Car car){
         Car car1 = service.updateCar(id, car);
